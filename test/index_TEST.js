2924631ab6ca0abab05e96251e9713043847c8ce
asciidisco <public@asciidisco.com>
2013-07-09 13:50:08 +0200

    Tests

---
+++ test/index_TEST.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect;
+var Dalek = require('../index.js');
+
+describe('dalek', function() {
+
+  it('should exist', function(){
+    expect(Dalek).to.be.ok;
+  });
+
+});
