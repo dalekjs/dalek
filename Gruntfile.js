6cbf752040b692345c4ba54e573651d83fe3fc07
asciidisco <public@asciidisco.com>
2013-10-16 18:34:42 +0200

    chore(gruntfile): Refactored Gruntfile to use dalek-build-tools

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,13 @@
-/* jshint camelcase: false */
 module.exports = function(grunt) {
   'use strict';
 
   // check task runtime
   require('time-grunt')(grunt);
 
+  // load generic configs
+  var configs = require('dalek-build-tools');
+
+  // project config
   grunt.initConfig({
 
     // load module meta data
@@ -19,320 +22,90 @@ module.exports = function(grunt) {
     },
 
     // clean automatically generated helper files & docs
-    clean: {
-      coverage: ['coverage', 'report/coverage'],
-      report: ['report/complexity', 'report/api', 'report/docs'],
-      reportZip: ['report.zip']
-    },
+    clean: configs.clean,
 
     // speed up build by defining concurrent tasks
-    concurrent: {
-      test: ['lint', 'mochaTest', 'complexity'],
-      docs: ['plato', 'yuidoc']
-    },
+    concurrent: configs.concurrent,
 
     // linting
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: '<%= src.lint %>'
-    },
+    jshint: configs.jshint,
 
     // testing
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec',
-          require: 'coverage/blanket'
-        },
-        src: '<%= src.test %>'
-      },
-      coverage: {
-        options: {
-          reporter: 'html-cov',
-          quiet: true,
-          captureFile: 'report/coverage/index.html'
-        },
-        src: '<%= src.test %>'
-      },
-      jsoncoverage: {
-        options: {
-          reporter: 'json-cov',
-          quiet: true,
-          captureFile: 'report/coverage/coverage.json'
-        },
-        src: '<%= src.test %>'
-      }
-    },
+    mochaTest: configs.mocha,
 
     // code metrics
-    complexity: {
-      generic: {
-        src: '<%= src.complexity %>',
-        options: {
-          cyclomatic: 10,
-          halstead: 23,
-          maintainability: 80
-        }
-      }
-    },
-    plato: {
-      generic: {
-        options : {
-          jshint : grunt.file.readJSON('.jshintrc')
-        },
-        files: {
-          'report/complexity': '<%= src.complexity %>',
-        }
-      }
-    },
+    complexity: configs.complexity,
+    plato: configs.plato(grunt.file.readJSON('.jshintrc')),
 
     // api docs
-    yuidoc: {
-      compile: {
-        name: '<%= pkg.name %>',
-        description: '<%= pkg.description %>',
-        version: '<%= pkg.version %>',
-        url: '<%= pkg.homepage %>',
-        options: {
-          paths: '.',
-          outdir: 'report/api'
-        }
-      }
-    },
+    yuidoc: configs.yuidocs(),
 
     // up version, tag & commit
-    bump: {
-      options: {
-        files: ['package.json'],
-        commit: true,
-        commitMessage: 'Release v%VERSION%',
-        commitFiles: ['package.json'],
-        createTag: true,
-        tagName: '%VERSION%',
-        tagMessage: '%VERSION%',
-        push: true,
-        pushTo: 'git@github.com:dalekjs/dalek.git'
-      }
-    },
+    bump: configs.bump({
+      pushTo: 'git@github.com:dalekjs/dalek.git',
+      files: ['package.json', 'CONTRIBUTORS.md', 'CHANGELOG.md']
+    }),
 
     // generate contributors file
-    contributors: {
-      master: {
-        path: 'CONTRIBUTORS.md',
-        branch: 'master'
+    contributors: configs.contributors,
+
+    // compress artifacts
+    compress: configs.compress,
+
+    // prepare files for grunt-plato to
+    // avoid error messages (weird issue...)
+    preparePlato: {
+      options: {
+        folders: [
+          'coverage',
+          'report',
+          'report/coverage',
+          'report/complexity',
+          'report/complexity/files',
+          'report/complexity/files/index_js'
+        ],
+        files: [
+          'report.history.json',
+          'files/index_js/report.history.json'
+        ]
       }
     },
 
-    // compress artifacts
-    compress: {
-      main: {
-        options: {
-          archive: 'report.zip'
-        },
-        files: [
-          {src: ['report/**'], dest: '/'}
-        ]
+    // prepare files & folders for coverage
+    prepareCoverage: {
+      options: {
+        folders: ['coverage', 'report', 'report/coverage'],
+        pattern: '[require("fs").realpathSync(__dirname + "/../index.js")]'
+      }
+    },
+
+    // list requires that need to be changed
+    // for generating a canary build
+    releaseCanary: {
+      options: {
+        files: ['index.js']
       }
     }
 
-  });
-
-  // prepare files & folders for grunt:plato & coverage
-  grunt.registerTask('preparePlato', function () {
-    var fs = require('fs');
-
-    // generate dirs for docs & reports
-    ['coverage', 'report', 'report/coverage',
-    'report/complexity', 'report/complexity/files',
-    'report/complexity/files/index_js'].forEach(function (path) {
-      if (!fs.existsSync(__dirname + '/' + path)) {
-        fs.mkdirSync(__dirname + '/' + path);
-      }
-    });
-
-    // store some dummy reports, so that grunt plato doesnt complain
-    ['report.history.json', 'files/index_js/report.history.json'].forEach(function (file) {
-      if (!fs.existsSync(__dirname + '/report/complexity/' + file)) {
-        fs.writeFileSync(__dirname + '/report/complexity/' + file, '{}');
-      }
-    });
-  });
-
-  // prepare files & folders for coverage
-  grunt.registerTask('prepareCoverage', function () {
-    var fs = require('fs');
-
-    // generate folders
-    ['coverage', 'report', 'report/coverage'].forEach(function (folder) {
-      if (!fs.existsSync(__dirname + '/' + folder)) {
-        fs.mkdirSync(__dirname + '/' + folder);
-      }
-    });
-
-    // generate code coverage helper file
-    var coverageHelper = 'require("blanket")({pattern: [require("fs").realpathSync(__dirname + "/../index.js")]});';
-    if (!fs.existsSync(__dirname + '/coverage/blanket.js')) {
-      fs.writeFileSync(__dirname + '/coverage/blanket.js', coverageHelper);
-    }
-  });
-
-  // generates a coverage badge
-  grunt.registerTask('generateCoverageBadge', function () {
-    var fs = require('fs');
-    if (fs.existsSync(__dirname + '/node_modules/coverage-badge')) {
-      if (fs.existsSync(__dirname + '/report/coverage/coverage.json')) {
-        var green = [147,188,59];
-        var yellow = [166,157,0];
-        var red = [189,0,2];
-
-        var getColor = function (coverage) {
-          if (coverage > 90) {
-            return mixColors(yellow, green, (coverage-90)/10);
-          }
-
-          if (coverage > 80) {
-            return mixColors(red, yellow, (coverage-80)/10);
-          }
-
-          return createColor(red);
-        };
-
-        var mixColors = function (from, to, ratio) {
-          var result = [], i;
-          for (i=0; i<3; i++) {
-            result[i] = Math.round(from[i] + (ratio * (to[i]-from[i])));
-          }
-          return createColor(result);
-        };
-
-        var createColor = function (values) {
-          return 'rgba('+values[0]+','+values[1]+','+values[2]+',1)';
-        };
-
-        var Badge = require(__dirname + '/node_modules/coverage-badge/lib/Badge.js');
-        var badgeFn = function(coverage) {
-          coverage = Math.floor(Number(coverage));
-          var badge = new Badge({
-            box_color: getColor(coverage),
-            box_text: coverage+'%',
-            label_text: 'cov',
-            height: 18,
-            width: 49,
-            box_width: 25,
-            rounding: 0,
-            padding: 0,
-            label_font: '7pt DejaVu Sans',
-            box_font: 'bold 7pt DejaVu Sans'
-          });
-          return badge.stream();
-        };
-
-        var coverage = JSON.parse(fs.readFileSync(__dirname + '/report/coverage/coverage.json')).coverage;
-        var file = fs.createWriteStream(__dirname + '/report/coverage/coverage.png');
-        badgeFn(coverage).pipe(file);
-      }
-    }
-  });
-
-  // releases a new canary build
-  grunt.registerTask('release-canary', function () {
-    var done = this.async();
-    var pkg = grunt.config.get('pkg');
-    var canaryPkg = grunt.util._.clone(pkg);
-
-    Object.keys(canaryPkg.dependencies).forEach(function (pack) {
-      if (pack.search('dalek') !== -1) {
-        delete canaryPkg.dependencies[pack];
-        canaryPkg.dependencies[pack + '-canary'] = 'latest';
-      }
-    });
-
-    canaryPkg.name = canaryPkg.name + '-canary';
-    canaryPkg.version = canaryPkg.version + '-' + grunt.template.today('yyyy-mm-dd-HH-MM-ss');
-
-    grunt.file.write('package.json', JSON.stringify(canaryPkg, true, 2));
-
-    // replace default includes with canary ones
-    var indexContents = grunt.file.read('index.js');
-    var oldContents = grunt.file.read('index.js');
-    indexContents = indexContents.replace('dalek-internal-driver', 'dalek-internal-driver-canary');
-    indexContents = indexContents.replace('dalek-internal-reporter', 'dalek-internal-reporter-canary');
-    indexContents = indexContents.replace('dalek-internal-timer', 'dalek-internal-timer-canary');
-    indexContents = indexContents.replace('dalek-internal-config', 'dalek-internal-config-canary');
-    grunt.file.write('index.js', indexContents);
-
-    var npm = require('npm');
-    npm.load({}, function() {
-      npm.registry.adduser(process.env.npmuser, process.env.npmpass, process.env.npmmail, function(err) {
-        if (err) {
-          grunt.log.error(err);
-          grunt.file.write('package.json', JSON.stringify(pkg, true, 2));
-          grunt.file.write('index.js', oldContents);
-          done(false);
-        } else {
-          npm.config.set('email', process.env.npmmail, 'user');
-          npm.commands.publish([], function(err) {
-            grunt.file.write('package.json', JSON.stringify(pkg, true, 2));
-            grunt.file.write('index.js', oldContents);
-            grunt.log.ok('Published canary build to registry');
-            done(!err);
-          });
-        }
-      });
-    });
-  });
-
-  // release a new version
-  grunt.registerTask('release-package', function () {
-    var done = this.async();
-    var http = require('http');
-    var pkg = grunt.config.get('pkg');
-    var body = '';
-
-    http.get('http://registry.npmjs.org/' + pkg.name, function(res) {
-      res.on('data', function (data) {
-        body += data;
-      });
-
-      res.on('end', function () {
-        var versions = grunt.util._.pluck(JSON.parse(body).versions, 'version');
-        var currVersion =  parseInt(pkg.version.replace(/\./gi, ''), 10);
-        var availableVersions = versions.map(function (version) {
-          return parseInt(version.replace(/\./gi, ''), 10);
-        });
-
-        if (!grunt.util._.contains(availableVersions, currVersion)) {
-          var npm = require('npm');
-          npm.load({}, function() {
-            npm.registry.adduser(process.env.npmuser, process.env.npmpass, process.env.npmmail, function(err) {
-              if (err) {
-                grunt.log.error(err);
-                done(false);
-              } else {
-                npm.config.set('email', process.env.npmmail, 'user');
-                npm.commands.publish([], function(err) {
-                  grunt.log.ok('Released new version: ', pkg.version);
-                  done(!err);
-                });
-              }
-            });
-          });
-        } else {
-          done();
-        }
-      });
-    });
   });
 
   // load 3rd party tasks
   require('load-grunt-tasks')(grunt);
+  grunt.loadTasks('./node_modules/dalek-build-tools/tasks');
 
   // define runner tasks
   grunt.registerTask('lint', 'jshint');
+
+  // split test & docs for speed
   grunt.registerTask('test', ['clean:coverage', 'prepareCoverage', 'concurrent:test', 'generateCoverageBadge']);
   grunt.registerTask('docs', ['clean:reportZip', 'clean:report', 'preparePlato', 'concurrent:docs', 'compress']);
+
+  // release tasks
+  grunt.registerTask('releasePatch', ['test', 'bump-before:patch', 'contributors', 'changelog', 'bump-release:patch']);
+  grunt.registerTask('releaseMinor', ['test', 'bump-before:minor', 'contributors', 'changelog', 'bump-release:minor']);
+  grunt.registerTask('releaseMajor', ['test', 'bump-before:major', 'contributors', 'changelog', 'bump-release:major']);
+
+  // clean, test, generate docs (the CI task)
   grunt.registerTask('all', ['clean', 'test', 'docs']);
 
 };
