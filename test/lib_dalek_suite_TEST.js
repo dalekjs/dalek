6a39757463d3198f046f371fe56ec32ae5bdc78e
asciidisco <public@asciidisco.com>
2013-10-22 20:32:57 +0200

    chore(test) Renamed testsuite unit tests to suite in order to match the renamed file in the lib/ folder

---
+++ test/lib_dalek_suite_TEST.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var expect = require('chai').expect;
+var Suite = require('../lib/dalek/suite.js');
+
+describe('dalek-internal-suite', function() {
+
+  it('should exist', function() {
+    expect(Suite).to.be.ok;
+  });
+
+});
