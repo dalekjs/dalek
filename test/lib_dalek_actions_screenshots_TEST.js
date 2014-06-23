062d8841d629dc355b37cf0c3fa209bb16937255
Skachov, Oleksandr <oleksandr.skachov@storebrand.no>
2014-06-23 18:38:13 +0200

    Using new library

---
+++ test/lib_dalek_actions_screenshots_TEST.js
@@ -0,0 +1,10 @@
+module.exports = {
+'Page title is correct': function (test) {
+  test
+    .open('http://localhost/ppjs/#/savings/index')
+    .screenshotE('test/screenshots/google2.png','.l-page-content-main')
+    //.assert.title().is('Google', 'It has title')
+    //.assert.screenshotIsEqualTo('test/screenshots/google.png')
+    .done();
+  }
+};
