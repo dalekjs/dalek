346c0953a998b267da0c2bafdcbbed78f57c74a2
gskachkov <gskachkov@gmail.com>
2014-06-27 03:21:03 +0300

    Fix comments from jshint

--- test/lib_dalek_actions_screenshots_TEST.js
+++ test/lib_dalek_actions_screenshots_TEST.js
@@ -1,10 +1,12 @@
 module.exports = {
-'Page title is correct': function (test) {
-  test
-    .open('http://localhost/ppjs/#/savings/index')
-    .screenshotE('test/screenshots/google2.png','.l-page-content-main')
-    //.assert.title().is('Google', 'It has title')
-    //.assert.screenshotIsEqualTo('test/screenshots/google.png')
-    .done();
+  'Page title is correct': function (test) {
+    'use strict';
+
+    test
+      .open('http://localhost/ppjs/#/savings/index')
+      .screenshotE('test/screenshots/google2.png','.l-page-content-main')
+      //.assert.title().is('Google', 'It has title')
+      //.assert.screenshotIsEqualTo('test/screenshots/google.png')
+      .done();
   }
 };
