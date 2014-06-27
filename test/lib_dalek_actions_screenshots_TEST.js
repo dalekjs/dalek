a46d38b62a9c5eab333455bca2f5473c6a3a3df8
gskachkov <gskachkov@gmail.com>
2014-06-27 22:41:09 +0300

    Add screenshot action spec

--- test/lib_dalek_actions_screenshots_TEST.js
+++ test/lib_dalek_actions_screenshots_TEST.js
@@ -1,12 +1,41 @@
-module.exports = {
-  'Page title is correct': function (test) {
-    'use strict';
+'use strict';
 
-    test
-      .open('http://localhost/ppjs/#/savings/index')
-      .screenshotE('test/screenshots/google2.png','.l-page-content-main')
-      //.assert.title().is('Google', 'It has title')
-      //.assert.screenshotIsEqualTo('test/screenshots/google.png')
-      .done();
-  }
-};
+var expect = require('chai').expect;
+var Actions = require('../lib/dalek/actions.js')({reporter: null});
+
+describe('dalek-internal-screenshot-action', function() {
+
+  var actions;
+  
+  beforeEach(function () {
+    actions = new Actions();
+    actions.actionPromiseQueue = [];
+  });
+
+  it('should exist', function() {
+    expect(actions).to.be.ok;
+  });
+
+  it('screenshot action should exist', function() {
+    expect(actions.screenshot).to.be.ok;
+  });
+
+  it('screenshot action should return link to this', function() {
+    expect(actions.screenshot('test/screenshot/file')).to.equal(actions);
+  });
+
+  it('screenshot action should add one callback function into action queue', function() {
+    expect(actions.actionPromiseQueue.length).to.equal(0);
+    actions.screenshot('test/screenshot/file');
+    expect(actions.actionPromiseQueue.length).to.equal(1);
+    expect(actions.actionPromiseQueue[0]).to.be.an('function');
+  });
+
+  it('screenshot action with element should add two callback functions into action queue', function() {
+    expect(actions.actionPromiseQueue.length).to.equal(0);
+    actions.screenshot('test/screenshot/file', '#lga');
+    expect(actions.actionPromiseQueue.length).to.equal(2);
+    expect(actions.actionPromiseQueue[1]).to.be.an('function');
+  });
+
+});
