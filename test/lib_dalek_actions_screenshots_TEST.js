ed50a6fdc8b90b01566a2847ca9087689d42d777
Sebastian Golasch <public@asciidisco.com>
2014-11-28 11:09:39 +0100

    fix(actions): better screenshot handling

---
+++ test/lib_dalek_actions_screenshots_TEST.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var expect = require('chai').expect;
+var Actions = require('../lib/dalek/actions.js')({reporter: null});
+
+describe('dalek-internal-screenshot-action', function() {
+
+  var actions;
+  
+  beforeEach(function () {
+    actions = new Actions();
+    actions.actionPromiseQueue = [];
+  });
+
+  it('should exist', function() {
+    expect(actions).to.be.ok;
+  });
+
+  it('screenshot action should exist', function() {
+    expect(actions.screenshot).to.be.ok;
+  });
+
+  it('screenshot action should return link to this', function() {
+    expect(actions.screenshot('test/screenshot/file')).to.equal(actions);
+  });
+
+  it('screenshot action should add one callback function into action queue', function() {
+    expect(actions.actionPromiseQueue.length).to.equal(0);
+    actions.screenshot('test/screenshot/file');
+    expect(actions.actionPromiseQueue.length).to.equal(1);
+    expect(actions.actionPromiseQueue[0]).to.be.an('function');
+  });
+
+  it('screenshot action with element should add two callback functions into action queue', function() {
+    expect(actions.actionPromiseQueue.length).to.equal(0);
+    actions.screenshot('test/screenshot/file', '#lga');
+    expect(actions.actionPromiseQueue.length).to.equal(2);
+    expect(actions.actionPromiseQueue[1]).to.be.an('function');
+  });
+
+});
